bafb4d47186725d67148eef0db64b32b364eca0c
Benjamin Boudra <benboudra@gmail.com>
2017-06-28 17:40:49 -0700

    added a basic test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn square(mut value_to_square: i32) -> i32 {
+    value_to_square *= value_to_square;
+    value_to_square
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn value_is_squared() {
+        assert_eq!(4, square(2))
+    }
+}
